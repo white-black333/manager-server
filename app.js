7f315556eb317e8271808625a04bae0c0fdd67f8
xccbbd <xccbbd@vae.com.cn>
2023-09-06 17:30:58 +0800

    feat:JWT实现token登录

--- app.js
+++ app.js
@@ -4,10 +4,14 @@ const views = require('koa-views');
 const json = require('koa-json');
 const onerror = require('koa-onerror');//处理响应错误
 const bodyparser = require('koa-bodyparser');//处理请求体
-// const logger = require('koa-logger');//输出服务日志
+const logger = require('koa-logger');//输出服务日志
 const log4js = require('./utils/log4j');//
 const index = require('./routes/index');
 const users = require('./routes/users');
+const router = require('koa-router')();
+const jwt = require('jsonwebtoken');
+const koajwt = require('koa-jwt');
+const util = require('./utils/util');
 
 // error handler  错误处理
 onerror(app);
@@ -17,7 +21,7 @@ app.use(bodyparser({
   enableTypes: ['json', 'form', 'text']
 }));
 app.use(json());
-// app.use(logger());
+app.use(logger());
 app.use(require('koa-static')(__dirname + '/public'));//处理静态资源
 
 app.use(views(__dirname + '/views', {
@@ -26,16 +30,37 @@ app.use(views(__dirname + '/views', {
 
 // logger 日志中间件
 app.use(async (ctx, next) => {  // 对于任何请求，app将调用该异步函数处理请求
-  await next();
+  await next().catch((err) => {       //next() 在注册的函数中调用用以执行下一个中间件
+    if (err.status == 401) {
+      ctx.status = 200;
+      ctx.body = util.fail('认证失败或TOKEN过期', util.CODE.AUTH_ERROR);
+    } else {
+      throw err;
+    }
+  });
 });
 
-// routes 路由
-app.use(index.routes(), index.allowedMethods());
-app.use(users.routes(), users.allowedMethods());
+app.use(koajwt({ secret: 'lxhbbd' }).unless({
+  path: '/api/users/login'
+}));//koajwt中间件 验证token的有效性 过滤login请求token验证
+
+router.prefix('/api');// routes 路由
+
+router.get('/leave/count', (ctx, next) => {
+  // 拿到请求头中的token数据
+  token = ctx.request.header.authorization.split(' ')[1];
+  // 验证token是否有效
+  const payload = jwt.verify(token, 'lxhbbd');
+  ctx.body = payload;
+});
+
+router.use(users.routes(), users.allowedMethods());
+
+app.use(router.routes(), router.allowedMethods()); //将koa - router对象的所有路由和处理函数注册成为中间件;
 
 // error-handling  全局错误处理
 app.on('error', (err, ctx) => {
-  log4js.error('error');
+  log4js.error(`${err.stack}`);
 });
 
 module.exports = app
--- routes/users.js
+++ routes/users.js
@@ -1,13 +1,33 @@
-const router = require('koa-router')()
+const router = require('koa-router')();
+const util = require('../utils/util');
+const User = require('./../models/userSchema');
+require('./../config/db');
+const jwt = require('jsonwebtoken');
 
-router.prefix('/users')
+router.prefix('/users');
 
-router.get('/', function (ctx, next) {
-  ctx.body = 'this is a users response!'
-})
+router.post('/login', async (ctx, next) => {
+  try {
+    console.log('ctx.request.body=>', ctx.request.body);
+    const { userName, userPwd } = ctx.request.body;
+    const res = await User.findOne({
+      userName,
+      userPwd
+    }, "userId userName userEmail state role deptId roleList");
+    const data = res._doc;
+    const token = jwt.sign({ data: data }, 'lxhbbd', { expiresIn: 30 });
+    console.log('token=>', token);
+    if (res) {
+      data.token = token;
+      console.log('res=>', res);
+      ctx.body = util.success(data);//ctx.response.body属性就是发送给用户的内容。
+      // ctx.body.data = token;
+    } else {
+      ctx.body = util.fail("账号或密码不正确");
+    }
+  } catch (error) {
+    ctx.body = util.fail(error.msg);
+  }
+});
 
-router.get('/bar', function (ctx, next) {
-  ctx.body = 'this is a users/bar response'
-})
-
-module.exports = router
+module.exports = router;
